fe2a1179cd1089807727f2a6016ea4251478aba1
alexander <san4ows55@gmail.com>
2026-03-23 17:59:21 +0300

    Lab 3: add blinker2

---
+++ lab_03_exceptions_and_interruptions/src/blinker2/blinker2.c
@@ -0,0 +1,20 @@
+#include <stm32h7xx.h>
+#include <stdio.h>
+#include <vterm.h>
+#include <user_leds.h>
+
+__attribute__((optimize("-O0"))) static void delay(int ms) {
+    volatile int counter = SystemCoreClock / 1000 / 6 * ms ;
+    while (counter > 0) counter -= 1;
+}
+
+int main() {
+    vterm_init(115200);
+    led_enable(led_green);
+    while(1){
+        static int counter = 0;
+        led_toggle(led_green);
+        printf("\r%s %d %s", "Led was toggled ", counter++, "time(s)");
+        delay(500);
+    };
+}
